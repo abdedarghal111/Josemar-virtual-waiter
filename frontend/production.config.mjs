e14d10e5a1ed1cdd1957e6870b4e273f59115107
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2025-06-08 19:09:46 +0200

    feat: added better configuration files

---
+++ frontend/production.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile  } from 'vite-plugin-singlefile';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const __shared = resolve(__dirname, '..', 'shared')
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(), 
+    svelte(),
+    viteSingleFile({ useRecommendedBuildConfig: false })
+  ],
+  resolve: {
+    'alias': {
+      '_shared': `${__shared}/src`,
+      "@src": resolve(__dirname, 'src', 'svelte')
+    }
+  },
+  build: {
+    target: 'modules',
+    outDir: '../dist/static',
+    assetsDir: 'public',
+    cssMinify: true,
+    minify: true,
+    emptyOutDir: true,
+    copyPublicDir: false,
+    cssCodeSplit: false,
+    assetsInlineLimit: 0
+  }
+})
